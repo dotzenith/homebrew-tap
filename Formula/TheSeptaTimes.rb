b00d1581c01f0b338e0cba6aceb1e8345e485ba7
dotzenith <sharmadanshu01@gmail.com>
2023-11-09 01:17:04 -0500

    update tst

--- Formula/TheSeptaTimes.rb
+++ Formula/TheSeptaTimes.rb
@@ -1,27 +1,27 @@
 class Theseptatimes < Formula
   desc "A CLI application for the SEPTA API "
   homepage "https://github.com/dotzenith/TheSeptaTimes.rs"
-  version "0.1.2"
+  version "0.2.0"
   license "MIT"
   
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/dotzenith/TheSeptaTimes.rs/releases/download/0.1.2/tst-0.1.2-x86_64-apple-darwin.tar.gz"
-    sha256 "e86e7cca3e8f5198c95cf78c33bb35128e21bb474f9278b998d03cc3289e97e5"
+    url "https://github.com/dotzenith/TheSeptaTimes.rs/releases/download/0.2.0/tst-0.2.0-x86_64-apple-darwin.tar.gz"
+    sha256 "13ddbdfe1b6dfdfebcf6a8a5f036bcd7f2c49361052d54b7a1aea4dff370889c"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/dotzenith/TheSeptaTimes.rs/releases/download/0.1.2/tst-0.1.2-aarch64-apple-darwin.tar.gz"
-    sha256 "82798b5827f494c2ae4134404a46f43ddceb10d50f4a02b6577489e0f4b627dd"
+    url "https://github.com/dotzenith/TheSeptaTimes.rs/releases/download/0.2.0/tst-0.2.0-aarch64-apple-darwin.tar.gz"
+    sha256 "4f8d1817ae529df31f419d8759065cf82a1e958a559336d639567f1b792f9073"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/dotzenith/TheSeptaTimes.rs/releases/download/0.1.2/tst-0.1.2-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "904282793c368604bb88bd46c4da0cb653e5fd00323f714366cb262facb3b5df"
+    url "https://github.com/dotzenith/TheSeptaTimes.rs/releases/download/0.2.0/tst-0.2.0-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "7ab3a104971468430366fb4abe71ede9b8bccadd26fc1227dc0702eed309a848"
   end
 
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/dotzenith/TheSeptaTimes.rs/releases/download/0.1.2/tst-0.1.2-arm-unknown-linux-gnueabihf.tar.gz"
-    sha256 "0a6b473200990a65df852bb550f03ea239af1e29be0177a5919aee08198a26f1"
+    url "https://github.com/dotzenith/TheSeptaTimes.rs/releases/download/0.2.0/tst-0.2.0-arm-unknown-linux-gnueabihf.tar.gz"
+    sha256 "1c73f511d3e2ae2856f7b6b6d4ec09a4b7f67722cbaa8a7d1b65ab76f4127b8c"
   end
 
   def install
