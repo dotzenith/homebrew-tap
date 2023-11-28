cbee154192e646f56bc9f82076756b21368ec99d
dotzenith <sharmadanshu01@gmail.com>
2023-11-27 19:02:54 -0500

    update tst 0.4.0

--- Formula/TheSeptaTimes.rb
+++ Formula/TheSeptaTimes.rb
@@ -1,27 +1,27 @@
 class Theseptatimes < Formula
   desc "A CLI application for the SEPTA API "
   homepage "https://github.com/dotzenith/TheSeptaTimes.rs"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
   
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/dotzenith/TheSeptaTimes.rs/releases/download/0.3.0/tst-0.3.0-x86_64-apple-darwin.tar.gz"
-    sha256 "fc0daa6aaf1bf82e27a341ba9832e53ffc89cae8fbfb108e8f0387128c1a7809"
+    url "https://github.com/dotzenith/TheSeptaTimes.rs/releases/download/0.4.0/tst-0.4.0-x86_64-apple-darwin.tar.gz"
+    sha256 "012686c044e865fd657d79604f3af56115b42dee53e6a68177491802decc1edb"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/dotzenith/TheSeptaTimes.rs/releases/download/0.3.0/tst-0.3.0-aarch64-apple-darwin.tar.gz"
-    sha256 "968c671a6c994502ad3dcff2f3fb4c9653a12c3b7034a44587b352d6a3c63d8a"
-  end
-
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/dotzenith/TheSeptaTimes.rs/releases/download/0.3.0/tst-0.3.0-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "a37cce5e859be1fc33644413ce406831ebb258f9148f13c43a689e934090465d"
+    url "https://github.com/dotzenith/TheSeptaTimes.rs/releases/download/0.4.0/tst-0.4.0-aarch64-apple-darwin.tar.gz"
+    sha256 "2717d6545fae646dc0e0cea41867b41e1b1bc486b762cec7130976193da9367f"
   end
 
   if OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/dotzenith/TheSeptaTimes.rs/releases/download/0.3.0/tst-0.3.0-arm-unknown-linux-gnueabihf.tar.gz"
-    sha256 "e9e8b2fa656554c34fa979b07f4960ed0829c37da078cdbaea7b15b8e3ea771a"
+    url "https://github.com/dotzenith/TheSeptaTimes.rs/releases/download/0.4.0/tst-0.4.0-arm-unknown-linux-gnueabihf.tar.gz"
+    sha256 "d40260345392619bcf439cde072cfa092067ca4a2ccfbb0ca1b263a5a7084010"
+  end
+
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/dotzenith/TheSeptaTimes.rs/releases/download/0.4.0/tst-0.4.0-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "ed1e9e0332d5453cfac83c7106533dd79afbff927de5d60e59a024a994757917"
   end
 
   def install
